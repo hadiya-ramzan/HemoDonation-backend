12d6f16e77a9cf6074ca378dce16ee7d086495a9
hadiya-ramzan <hadiaramzan3090@gmail.com>
2026-06-17 18:45:57 -0700

    app.js updated (CORS issue fix)

--- src/app.js
+++ src/app.js
@@ -18,12 +18,17 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://hemo-donation-frontend.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://hemo-donation-frontend.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -31,7 +36,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 //  static files 
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
-  //  ROOT ROUTE (FIX IMPORTANT)
+  //  ROOT ROUTE 
 
 app.get("/", (req, res) => {
   res.json({
