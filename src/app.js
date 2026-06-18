b5b61529170ad67737a3d317b7a5cade6c7a3007
hadiya-ramzan <hadiaramzan3090@gmail.com>
2026-06-17 21:00:10 -0700

    cors fix

--- src/app.js
+++ src/app.js
@@ -16,23 +16,14 @@ const notificationRoutes = require("./routes/notification.routes");
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "https://hemo-donation-frontend.vercel.app"
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
 app.use(cors({
-  origin: true,
+  origin: [
+    "http://localhost:5173",
+    "https://hemo-donation-frontend.vercel.app"
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json({ limit: "10mb" }));
